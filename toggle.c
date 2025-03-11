889c57928caede9d35d0adaff594429f9d85accf
Mamatha <madhanalamamatha9@gmail.com>
2025-03-11 16:53:09 +0530

    removed

--- toggle.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-	int num,toggle_bit;
-	printf("enter the number and enter the toggle bit\n");
-	scanf("%d%d",&num,&toggle_bit);
-	num=num^(1<<toggle_bit);
-	printf("after toggling the bits the result is:%d\n",num);
-}
