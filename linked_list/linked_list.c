3bb42173545d59c3ab5617b49d655a8c0be574b9
mamatha <madhanalamamatha>
2025-07-02 16:52:30 +0530

    simple linked list and display

---
+++ linked_list/linked_list.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+typedef struct list{
+ int data;
+ struct list *next;
+}list;
+
+struct list *head=NULL, *tail=NULL;
+
+
+list* create_node(int n)
+{
+list *temp;
+temp=(list*)malloc(sizeof(list));
+if(temp==NULL){
+ return NULL;
+}
+else{
+temp->data=n;
+temp->next=NULL;
+return temp;
+}
+}
+
+void insert(list* temp)
+{
+	if(head==NULL){
+		head=temp;
+		tail=temp;
+	}
+	else{
+		tail->next=temp;
+		tail=temp;
+	}
+}
+
+void display()
+{
+	for(list *t=head;t!=NULL;t=t->next){
+		printf("%d->",t->data);
+         //	printf("%p ",t->next);
+	//	printf("%p ",&t->data);
+}
+printf("\n");
+}
+
+int main(){
+int var,dt;
+list *t;
+printf("enter the number nodes u want\n");
+scanf("%d",&var);
+for(int i=1;i<=var;i++)
+{
+printf("enter the data in %d node\n",i);
+scanf("%d",&dt);
+t=create_node(dt);
+insert(t);
+}
+display();
+
+return 0;
+}
